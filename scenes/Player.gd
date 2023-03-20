120cf3ee2992780057418881f36211d52d9a45da
Mario Recabarren Díaz <77750374+Milwer7@users.noreply.github.com>
2023-03-20 17:12:40 -0300

    feat!: player topdown movement & resizable window

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,28 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300
+@export var acceleration = 4000
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func move_character(delta):
+	# Moves the character based on the input on both axis
+	var move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
+	velocity.x = move_toward(velocity.x, move_input.x * speed, acceleration * delta)
+	velocity.y = move_toward(velocity.y, move_input.y * speed, acceleration * delta)
+	move_and_slide()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	move_character(delta) # Moving the character
